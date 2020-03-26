e86ec823bb554247fa30a0cbca46fc0de3a42a8b
Shubhankar <shubhankar.andi5@gmail.com>
2020-03-27 00:36:43 +0530

    take port from heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const app=express()
 
+const server_port = process.env.PORT || 6111
 app.use('/public', express.static('./front'))
 
 app.get('/', (req, res) => res.send("Hello World Backend."))
-app.listen(6111)
+app.listen(server_port)
